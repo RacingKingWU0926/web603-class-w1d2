0af2e26b9c1edac8ca3828d6831baae57d011805
JasonLei <jlei@activision.com>
2024-01-10 19:27:08 -0800

    completed demo5

--- demo5.js
+++ demo5.js
@@ -1,3 +1,14 @@
+// create the Football class
+class Football extends React.Component {
+  shoot = () => {
+    alert(this);  // this will print the object 
+  }
 
+  render() {
+    return (
+      <button onClick={this.shoot}>Take the shot!</button>
+    );
+  }
+}
 
 ReactDOM.render(<Football />, document.getElementById('root'));
