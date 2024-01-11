ea206f456bcdde6812eb6628abc6acaa78d52f8d
JasonLei <jlei@activision.com>
2024-01-10 19:15:35 -0800

    completed demo2

--- demo2.js
+++ demo2.js
@@ -8,18 +8,18 @@ class Car extends React.Component {
         year: 1964
       };
     }
-    //add changeColor function here
+    // add changeColor function here
+    changeColor = () => {
+      this.setState({color: "blue "})
+    }
 
     render() {
       return (
         <div>
           <h1>My {this.state.brand}</h1>
-          <p>
-            It is a {this.state.color}
-            {this.state.model}
-            from {this.state.year}.
-          </p>
-          
+          <p>It is a {this.state.color} {this.state.model} from {this.state.year}.</p>
+          {/* create a button */}
+          <button type="button" onClick={this.changeColor}>Change color!</button>
         </div>
       );
     }
